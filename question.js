d5379cbaed57046141ff49762c09c595f613d146
Tejasvairal <126770816+Tejasvairal@users.noreply.github.com>
2023-03-03 10:51:19 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,79 @@
+let MCQS = [{
+    question: "which language runs in a web browser?",
+    choice1: "Java",
+    choice2: "C",
+    choice3: "HTML",
+    choice4: "Javascrip",
+    answer: 3
+},
+            {
+            question: "Whichg of the following is used to create web pages?",
+    choice1: "C",
+    choice2: "HTML",
+    choice3: "Java script",
+    choice4: "Css",
+    answer: 1
+},
+            {
+            question: "Opening tag of HTML is called?",
+    choice1: "Ending tage",
+    choice2: "Pair tags",
+    choice3: "Closed tag",
+    choice4: "Starting tag",
+    answer: 3
+},
+            {
+            question: "Who perfroms the most on-screen kills in the Lord of The Rings movies?",
+    choice1: "Boromir",
+    choice2: "Gimli",
+    choice3: "Legolas",
+    choice4: "Frodo",
+    answer: 2
+},
+            {
+            question: "How many Oscars did the 1994 movie Forrest Gump win?",
+    choice1: "3 oscars",
+    choice2: "9 oscars",
+    choice3: "6 oscars",
+    choice4: "12 oscars",
+    answer: 2
+},
+            {
+            question: "In which form did 'The Jungle Book' originally appear?In which form did 'The Jungle Book' originally appear?",
+    choice1: "Radio Play",
+    choice2: "Film",
+    choice3: "Book",
+    choice4: "An app",
+    answer: 2
+},
+            {
+               question: "Which city is the setting for the 1950s film musical Gigi?",
+    choice1: "London",
+    choice2: "Paris",
+    choice3: "New York",
+    choice4: "Rio de Janeiro",
+    answer: 1
+},{
+               question: "What world-famous movie monster turned 50 in 2005?",
+    choice1: "Frankenstein",
+    choice2: "King Kong",
+    choice3: "Mothra",
+    choice4: "Godzilla",
+    answer: 3
+},
+            {
+               question: "what year was JavaScript launched?",
+    choice1: "1996",
+    choice2: "1995",
+    choice3: "1994",
+    choice4: "none of this above",
+    answer: 1
+},
+            {
+               question: "Which of the following is not javascript data types?",
+    choice1: "Undefined type",
+    choice2: "Number type",
+    choice3: "Null type",
+    choice4: "All of the mentioned",
+    answer: 3
+}];
